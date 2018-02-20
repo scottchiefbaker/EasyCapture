360c1a65bf277ae9355a6f852545a179872b9529
Scott Baker <scott@perturb.org>
2018-02-20 10:51:46 -0800

    Only show the menu on click

--- js/jquery.dropdownPlain.js
+++ js/jquery.dropdownPlain.js
@@ -1,17 +1,17 @@
 $(function(){
+	// Show the menu on click (You can use "mouseover mouseout" here for the hover effect
+	$("ul.dropdown li").on("click",function() {
+		var is_vis = $('ul:first',this).css('visibility');
+		console.log(is_vis);
 
-    $("ul.dropdown li").hover(function(){
-    
-        $(this).addClass("hover");
-        $('ul:first',this).css('visibility', 'visible');
-    
-    }, function(){
-    
-        $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
-    
-    });
-    
-    $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+		if (is_vis =="hidden") {
+			$(this).addClass("hover");
+			$('ul:first',this).css('visibility', 'visible');
+		} else {
+			$(this).removeClass("hover");
+			$('ul:first',this).css('visibility', 'hidden');
+		}
+	});
 
+	$("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
 });
