cd0c323a9a6080d5f8a6583d20c0f0a97e488893
Scott Baker <scott@perturb.org>
2018-02-20 16:01:59 -0800

    Hide other menus if you open a new one

--- js/jquery.dropdownPlain.js
+++ js/jquery.dropdownPlain.js
@@ -4,11 +4,14 @@ $(function(){
 		var is_vis = $('ul:first',this).css('visibility');
 		console.log(is_vis);
 
+		// Show the menu
 		if (is_vis =="hidden") {
-			$(this).addClass("hover");
+			// Hide all the other menus in case one got left open
+			$('.action_menu ul').css('visibility', 'hidden');
+
 			$('ul:first',this).css('visibility', 'visible');
+		// Hide the menu
 		} else {
-			$(this).removeClass("hover");
 			$('ul:first',this).css('visibility', 'hidden');
 		}
 	});
